3bb9bc7e2759908b5289d882f18d906f832bc594
Ilya <IlyaAbdullinWork@yandex.ru>
2022-04-04 17:19:37 +0500

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
+import type { FC } from 'react'
 import { tokens, categories } from './tokens'
 import { menu } from './menu'
 import './App.css'
 
-function App() {
+import { Provider, observer } from 'mobx-react'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import { store } from './store'
+
+export const App: FC = observer(() => {
   const [category, setCategory] = useState(categories[0])
 
   return (
-    <div className="app">
-      <div className="menu">
-        {menu.map((item) => (
-          <div className="menu-item">{item.title}</div>
-        ))}
+    <Provider store={store}>
+      <div className="app">
+        <div className="menu">
+          {menu.map((item) => (
+            <div className="menu-item">{item.title}</div>
+          ))}
+        </div>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <div className="tokens">
+                  {tokens.map((item) => (
+                    <div className="token">{item.name}</div>
+                  ))}
+                </div>
+              }
+            />
+            <Route path={`/search`} element={<div>123</div>} />
+          </Routes>
+        </BrowserRouter>
       </div>
-      <div className="tokens">
-        {tokens.map((item) => (
-          <div className="token">{item.name}</div>
-        ))}
-      </div>
-    </div>
+    </Provider>
   )
-}
+})
 
 export default App
